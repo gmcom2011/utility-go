749cf20d43b632d0be0430d6699b287dc4b0f714
ณัฐวัฒน์ วิชชุวรรณ <t641017@tcrbank.com>
2022-10-28 20:54:22 +0700

    []

--- sub/sub.go
+++ sub/sub.go
@@ -3,8 +3,10 @@ package sub
 import (
 	"rsc.io/quote"
 	"fmt"
+	"github.com/gmcom2011/utility-go/sub_sub"
 )
 
 func Sub() {
+    fmt.Println("Hello SubSub", sub_sub.SubSub())
     fmt.Println("Hello Sub",quote.Go())
 }
